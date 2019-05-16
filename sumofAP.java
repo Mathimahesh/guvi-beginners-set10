2709bd07bd702cd3976bb7bb4fad6d5f2200be4f
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-05-16 05:38:29 -0700

    Create sumofAP.java

---
+++ sumofAP.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+       int c = sc.nextInt();
+       int value = (c/2)*(2*a+(c-1)*b);
+       System.out.println(value); 
+       
+       
+    }
+}
